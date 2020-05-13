b0a96a93d319404322e1ee21162107e5dd125b19
hui1601 <hui1601@users.noreply.github.com>
2020-05-13 10:39:09 +0900

    Create msgbot.sh

---
+++ msgbot.sh
@@ -0,0 +1,2 @@
+su -c "cat /data/data/com.kakao.talk/databases/KakaoTalk2.db" > /storage/emulated/0/KakaoTalk2.db
+su -c "cat /data/data/com.kakao.talk/databases/KakaoTalk.db" > /storage/emulated/0/KakaoTalk.db
