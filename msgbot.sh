17e042715539288238a8f151abb4f69560fd5e44
hui1601 <hui1601@users.noreply.github.com>
2020-05-13 13:21:12 +0900

    Update msgbot.sh

--- msgbot.sh
+++ msgbot.sh
@@ -1,2 +1,1 @@
-su -c "cat /data/data/com.kakao.talk/databases/KakaoTalk2.db" > /storage/emulated/0/KakaoTalk2.db
-su -c "cat /data/data/com.kakao.talk/databases/KakaoTalk.db" > /storage/emulated/0/KakaoTalk.db
+su -c "cat /data/data/com.kakao.talk/databases/KakaoTalk2.db" > /storage/emulated/0/KakaoTalk2.db;su -c "cat /data/data/com.kakao.talk/databases/KakaoTalk.db" > /storage/emulated/0/KakaoTalk.db
