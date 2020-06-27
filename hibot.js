31d9bced10f6131c15907812661a63a2db5cb220
hui1601 <hui1601@users.noreply.github.com>
2020-06-27 20:08:43 +0900

    Update hibot.js

--- hibot.js
+++ hibot.js
@@ -282,28 +282,28 @@ DatabaseWatcher.prototype = {
 										Log.d(obj.message);
 										let room = getRoomName(obj.chat_id);
 										let send_username = getUserInfo(obj.user_id, "name");
-										if(send_username == null) send_username = "";
-										else if(obj.v.origin == "KICKMEM") send_username = send_username + "님이 ";
-										else send_username = send_username + "님 ";
+										/*if(send_username == null) */send_username = "";
+										//else if(obj.v.origin == "KICKMEM") send_username = send_username + "님이 ";
+										//else send_username = send_username + "님 ";
 										if (obj.v.origin == "NEWMEM")
 											Api.replyRoom(room, send_username + "안녕하세요! 공지에 있는 규칙 필독해주세요.");
 										else if (obj.v.origin == "DELMEM" && JSONObject(obj.message).get("feedType") == 2)
 											Api.replyRoom(room, send_username + "안녕히가세요!");
 										else if (obj.v.origin == "KICKMEM" || obj.v.origin == "DELMEM"){
 											obj.message = new JSONObject(obj.message);
-											let by = getUserInfo(obj.message.get("member").getString("userId"), "name");
-											if(by == null) by = "";
-											else by = by + "님을 ";
+											//let by = getUserInfo(obj.message.get("member").getString("userId"), "name");
+											/*if(by == null) */by = "";
+											//else by = by + "님을 ";
 											if(by == "" && send_username == "") Api.replyRoom(room, "다음부턴 착하게 사세요!");
 											else Api.replyRoom(room, send_username + by + "강퇴하였습니다. 다음부턴 착하게 사세요!");
 										}
-										else if (obj.type == 26 && obj.message == "who") {
+										/*else if (obj.type == 26 && obj.message == "who") {
 											obj.attachment = new JSONObject(decrypt(obj.user_id, obj.v.enc, obj.attachment));
 											let userid = obj.attachment.getString("src_userId");
 											Api.replyRoom(room, "이름: "+getUserInfo(userid, "name")
 											+"\n프로필 사진: "+getUserInfo(userid, "original_profile_image_url")
 											+"\n상태 메시지: "+getUserInfo(userid, "status_message"));
-										}
+										}*/
 										else if (obj.type == 26 && obj.message == "photolink") {
 											obj.attachment = new JSONObject(decrypt(obj.user_id, obj.v.enc, obj.attachment));
 											if(obj.attachment.get("src_type") != 2)
