b77b2f1d4aa377378286e555f456499a84a2f21d
Hibot <hui1601@naver.com>
2020-08-30 01:07:02 +0900

    Update hibot.js

--- hibot.js
+++ hibot.js
@@ -25,7 +25,7 @@ const SecretKeyFactory = javax.crypto.SecretKeyFactory;
 const SecretKeySpec = javax.crypto.spec.SecretKeySpec;
 
 const JSONObject = org.json.JSONObject;
-
+const package = "com.kakao.talk"//DB를 읽을 패키지 명
 const MY_KEY = "298920935";//KakaoTalk2.db->open_profile->user_id(반드시 바꿔주세요!)
 const Loading_cycle = 1000;//불러오는 주기 ms단위(1초=1000ms)
 var powerManager = Api.getContext().getSystemService(Context.POWER_SERVICE);
@@ -58,41 +58,30 @@ function decrypt(userId, enc, text) {
 		cipher.init(2, secretKeySpec, ivParameterSpec);
 		return String(new _String(cipher.doFinal(Base64.decode(text, 0)), "UTF-8"));
 	} catch (e) {
-		Log.error(e.lineNumber+": "+e);
+		Log.error(e.lineNumber + ": " + e);
 		return null;
 	}
 }
-function copy_DB() {
+function requirePermission() {
 	try {
-		var cmd = new ArrayList();
-		cmd.add("msgbot.sh");
-		var ps = new ProcessBuilder(cmd);
-		ps.redirectErrorStream(true);
-		var pr = ps.start();
-		var in1 = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-		var line;
-		while ((line = in1.readLine()) != null) {
-			Log.d(line);
-		}
-		pr.waitFor();
-		in1.close();
+		Runtime.getRuntime().exec(["su", "chmod -R 777 /data/data/" + package + "/databases"]);
 		return true;
 	} catch (e) {
-		Log.error(e.lineNumber+": "+e);
+		Log.error(e.lineNumber + ": " + e);
 		return false;
 	}
 }
-function connect_DB() {
-	DB_copy();
+function connectDB() {
+	requirePermission();
 	try {
-		var kakao1="/storage/emulated/0/KakaoTalk.db", kakao2="/storage/emulated/0/KakaoTalk2.db";
+		var kakao1 = "/data/data/" + package + "/databases/KakaoTalk.db", kakao2 = "/data/data/" + package + "/databases/KakaoTalk.db";
 		if(db != null) db.close();
 		if(db2 != null) db2.close();
-		db = SQLiteDatabase.openDatabase(kakao1, null, 0);
-		db2 = SQLiteDatabase.openDatabase(kakao2, null, 0);
+		db = SQLiteDatabase.openDatabase(kakao1, SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING, 0);
+		db2 = SQLiteDatabase.openDatabase(kakao2, SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING, 0);
 		return true;
 	} catch (e) {
-		Log.error(e.lineNumber+": "+e);
+		Log.error(e.lineNumber + ": " + e);
 		return false;
 	}
 }
@@ -131,7 +120,7 @@ function getRecentChatData(count) {
 		cursor.close();
 		return data;
 	} catch (e) {
-		Log.error(e.lineNumber+": "+e);
+		Log.error(e.lineNumber + ": " + e);
 		return null;
 	}
 }
@@ -152,7 +141,7 @@ function getRoomName(chat_id) {
 		}
 		return room;
 	} catch (e) {
-		Log.error(e.lineNumber+": "+e);
+		Log.error(e.lineNumber + ": " + e);
 		return null;
 	}
 }
@@ -282,28 +271,28 @@ DatabaseWatcher.prototype = {
 										Log.d(obj.message);
 										let room = getRoomName(obj.chat_id);
 										let send_username = getUserInfo(obj.user_id, "name");
-										/*if(send_username == null) */send_username = "";
-										//else if(obj.v.origin == "KICKMEM") send_username = send_username + "님이 ";
-										//else send_username = send_username + "님 ";
+										if(send_username == null) send_username = "";
+										else if(obj.v.origin == "KICKMEM") send_username = send_username + "님이 ";
+										else send_username = send_username + "님 ";
 										if (obj.v.origin == "NEWMEM")
 											Api.replyRoom(room, send_username + "안녕하세요! 공지에 있는 규칙 필독해주세요.");
 										else if (obj.v.origin == "DELMEM" && JSONObject(obj.message).get("feedType") == 2)
 											Api.replyRoom(room, send_username + "안녕히가세요!");
 										else if (obj.v.origin == "KICKMEM" || obj.v.origin == "DELMEM"){
 											obj.message = new JSONObject(obj.message);
-											//let by = getUserInfo(obj.message.get("member").getString("userId"), "name");
-											/*if(by == null) */by = "";
-											//else by = by + "님을 ";
+											let by = getUserInfo(obj.message.get("member").getString("userId"), "name");
+											if(by == null) by = "";
+											else by = by + "님을 ";
 											if(by == "" && send_username == "") Api.replyRoom(room, "다음부턴 착하게 사세요!");
 											else Api.replyRoom(room, send_username + by + "강퇴하였습니다. 다음부턴 착하게 사세요!");
 										}
-										/*else if (obj.type == 26 && obj.message == "who") {
+										else if (obj.type == 26 && obj.message == "who") {
 											obj.attachment = new JSONObject(decrypt(obj.user_id, obj.v.enc, obj.attachment));
 											let userid = obj.attachment.getString("src_userId");
 											Api.replyRoom(room, "이름: "+getUserInfo(userid, "name")
 											+"\n프로필 사진: "+getUserInfo(userid, "original_profile_image_url")
 											+"\n상태 메시지: "+getUserInfo(userid, "status_message"));
-										}*/
+										}
 										else if (obj.type == 26 && obj.message == "photolink") {
 											obj.attachment = new JSONObject(decrypt(obj.user_id, obj.v.enc, obj.attachment));
 											if(obj.attachment.get("src_type") != 2)
@@ -318,14 +307,14 @@ DatabaseWatcher.prototype = {
 											cursor.close();
 											let photo = decrypt(userId1, getUserInfo(userId1, "enc"), msg1);
 											photo = new JSONObject(photo);
-											Api.replyRoom(room, "링크: "+photo.get("url"));
+											Api.replyRoom(room, "링크: " + photo.get("url"));
 										}
 									}
 								}
 							}
 						}
 					} catch (e) {
-						Log.error(e.lineNumber+": "+e);
+						Log.error(e.lineNumber + ": " + e);
 					}
 				}
 			}), 0, Loading_cycle);
@@ -350,20 +339,12 @@ function onStartCompile() {
 	watcher.stop();
 }
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-	if(msg == "!"+scriptName+"-off")
-	{
+	if(msg == "!"+scriptName+"-off"){
 		replier.reply(scriptName+"을(를) 종료합니다.");
 		watcher.stop();
 		Api.off(scriptName);
 	}
-	else if(msg == "!bot-reply-test")
-	{
+	else if(msg == "!bot-reply-test"){
 		replier.reply("can reply");
 	}
 }
-function onCreate(savedInstanceState, activity) {
-}
-function onStart(activity) {}
-function onResume(activity) {}
-function onPause(activity) {}
-function onStop(activity) {}
